fb3f7ac42ade0f157efee84d1b54e7fa2410a526
josue.0 <josueajc@gmail.com>
2023-08-20 05:28:30 -0500

    test: test removed

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,5 @@
-import { myPackage } from '../src';
-
 describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
-
-      const result = myPackage(message);
-
-      expect(result).toMatch(message);
-    });
+  it('should pass', () => {
+    expect(true).toBe(true);
   });
 });
